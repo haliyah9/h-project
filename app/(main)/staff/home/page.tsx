bc143d677bdc4897723c43d57bd4a3c2dcb4a0b4
Emmy <famuyiwaemmanuel565@gmail.com>
2026-09-13 14:21:01 +0100

    Add link to submit attachments on staff home page

--- app/(main)/staff/home/page.tsx
+++ app/(main)/staff/home/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { useAuth } from "@/context/AuthContext";
+import Link from "next/link";
 
 const StaffHomePage = () => {
   const { handleSignout } = useAuth();
   return (
     <div>
       <h1>This is the staff home page</h1>
+      <Link href="/staff/submit-requests">Submit Attachments</Link>
       <button type="button" onClick={handleSignout}>
         Sign Out
       </button>
